78d33de2e05b4f757d5b8a5e99a2105a1dd3ce8d
jhow212 <santoscostaj808@gmail.com>
2022-10-31 19:51:08 -0300

    feat(nova pasta utils e o request):

---
+++ front-end/src/utils/Resquest.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+// Funções pra fazer o que a gente quer
+
+export const setToken = (token) => {
+  api.defaults.headers.common.Authorization = token;
+};
+
+export const requestGet = async (endpoint) => {
+  const { data } = await api.get(endpoint);
+  return data;
+};
+
+export const requestGetId = async (endpoint, id) => {
+  const { data } = await api.get(endpoint, { params: { id } });
+  return data;
+};
+
+export const requestPost = async (endpoint, body) => {
+  const { data } = await api.post(endpoint, body);
+  return data;
+};
+
+export default api;
