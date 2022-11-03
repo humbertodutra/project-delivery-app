c897f0dd4794585a695ef694a07a7fb557ae66b1
João Pster <joaopsterdev@gmail.com>
2022-11-03 18:37:27 -0300

    dev: adicionado a página de produtos

--- front-end/src/pages/Customer/Products/Products.jsx
+++ front-end/src/pages/Customer/Products/Products.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import Header from '../../../components/Header/Header';
 import Footer from '../../../components/Footer/Footer';
 import AppWrap from '../../../wrapper/AppWrap';
+import ProductCard from '../../../components/ProductCard/ProductCard';
 
 import { requestGet } from '../../../utils/Resquest';
 
@@ -19,7 +20,25 @@ function Products() {
   return (
     <div>
       {console.log(products)}
-      Products
+      {products ? (
+        <div>
+          {
+            products.map((product) => (
+              <ProductCard
+                key={ product.id }
+                id={ product.id }
+                name={ product.name }
+                price={ product.price }
+                urlImage={ product.urlImage }
+              />
+            ))
+          }
+        </div>
+      ) : (
+        <div>
+          <h1>Carregando...</h1>
+        </div>
+      )}
     </div>
   );
 }
