b1516f16cdd881dd5c5cee9ac034a17287a5051b
João Pster <joaopsterdev@gmail.com>
2022-11-07 15:50:30 -0300

    lint: resolvido erros

--- back-end/src/middlewares/authMiddleware.js
+++ back-end/src/middlewares/authMiddleware.js
@@ -1,8 +1,8 @@
 require('dotenv/config');
 const jwt = require('jsonwebtoken');
 
-const jwtKey = require("fs")
-  .readFileSync("./jwt.evaluation.key", { encoding: "utf-8" });
+const jwtKey = require('fs')
+  .readFileSync('./jwt.evaluation.key', { encoding: 'utf-8' });
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
